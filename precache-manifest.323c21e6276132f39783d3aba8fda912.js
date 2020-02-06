bb8f6e16302dfeabe20176d477b149549615d6e9
alavrentyev <alavrentyev@issart.com>
2020-02-06 11:53:28 +0600

    Updates

---
+++ precache-manifest.323c21e6276132f39783d3aba8fda912.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbfd3117d52a9cace97c2e58263d60ee",
+    "url": "./index.html"
+  },
+  {
+    "revision": "148fed9fb087f9f6296c",
+    "url": "./static/css/main.71cbc92e.chunk.css"
+  },
+  {
+    "revision": "7fe6c089c4fc54d3febe",
+    "url": "./static/js/2.55d4c28a.chunk.js"
+  },
+  {
+    "revision": "967c3aa677d48917bda81b3b2ff2693c",
+    "url": "./static/js/2.55d4c28a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "148fed9fb087f9f6296c",
+    "url": "./static/js/main.f4e1e83c.chunk.js"
+  },
+  {
+    "revision": "a80abd87409a8d13475b",
+    "url": "./static/js/runtime-main.e2194025.js"
+  },
+  {
+    "revision": "08e4338288e83eb1b1d81d48b158aedc",
+    "url": "./static/media/ip.08e43382.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-SPA-IP-checker/precache-manifest.587099f8c932444d0c716fcfbd09424f.js"
+  "./precache-manifest.323c21e6276132f39783d3aba8fda912.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-SPA-IP-checker/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
